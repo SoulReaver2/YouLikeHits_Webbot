09ce06bbf23a61dbd1f67d38025e7435baf06333
Your Name <you@example.com>
2024-02-20 18:16:58 +0100

    Introduce a time to live for saved cookies

--- libs/request.js
+++ libs/request.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 
 const COOKIES_PATH = "./cookies/cookies.json";
+const COOKIE_EXPIRATION_TIME_sec = 24 * 3600; // 24 hours in seconds
 
 async function initializePageWithCookies(browser, url) {
   const page = await browser.newPage();
@@ -11,9 +12,13 @@ async function initializePageWithCookies(browser, url) {
 
   await page.goto(url);
 
-  // save the new cookies for security and future use
-  const savedcookies = await page.cookies();
-  await fs.writeFile(COOKIES_PATH, JSON.stringify(savedcookies));
+  // save the new cookies for security and future use only if the old cookies expired
+  const { mtime } = await fs.stat(COOKIES_PATH);
+  const xHoursAgo = new Date(Date.now() - COOKIE_EXPIRATION_TIME_sec * 1000);
+  if (mtime < xHoursAgo) {
+    const savedcookies = await page.cookies();
+    await fs.writeFile(COOKIES_PATH, JSON.stringify(savedcookies));
+  }
   return page;
 }
 
